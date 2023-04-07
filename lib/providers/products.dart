b9993879f6c1f56f7cfe7887a2ce2ca918056d2f
帅比Crystal <51915895+CrystalCAI11@users.noreply.github.com>
2023-04-07 16:18:10 +0800

    use provider

---
+++ lib/providers/products.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import 'product.dart';
+
+class Products with ChangeNotifier {
+  final List<Product> _items = [
+    Product(
+      id: 'p1',
+      title: 'Red Shirt',
+      description: 'A red shirt - it is pretty red!',
+      price: 29.99,
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    ),
+    Product(
+      id: 'p2',
+      title: 'Trousers',
+      description: 'A nice pair of trousers.',
+      price: 59.99,
+      imageUrl:
+          'https://img2.baidu.com/it/u=887767646,637468536&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500',
+    ),
+    Product(
+      id: 'p3',
+      title: 'Yellow Scarf',
+      description: 'Warm and cozy - exactly what you need for the winter.',
+      price: 19.99,
+      imageUrl:
+          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    ),
+    Product(
+      id: 'p4',
+      title: 'A Pen',
+      description: 'Writing...',
+      price: 49.99,
+      imageUrl:
+          'https://www.montblanc.com/variants/images/34480784411792216/A/w828.jpg',
+    ),
+  ];
+
+  List<Product> get items {
+    return [..._items];
+  }
+
+  void addProduct(Product product) {
+    _items.add(product);
+    notifyListeners(); // 新增商品，products变化，通知listeners变化
+  }
+
+  Product findProductById(String id) {
+    return _items.firstWhere((prod) => prod.id == id);
+  }
+}
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/product_detail_screen.dart';
+import '../providers/product.dart';
+
+class ProductItem extends StatelessWidget {
+  // final String id;
+  // final String title;
+  // final String imageUrl;
+
+  const ProductItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          leading: IconButton(
+            icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border),
+            onPressed: () {
+              product.toggleFavorite();
+            },
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () => {},
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+        ),
+        child: GestureDetector(
+          // Image本身没有onTap方法，我们加一层GestureDetector来实现
+          onTap: () {
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                arguments: product.id);
+          },
+          child: Image.network(product.imageUrl, fit: BoxFit.cover),
+        ),
+      ),
+    );
+  }
+}
