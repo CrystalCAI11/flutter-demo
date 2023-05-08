4da0257f6d89efdae97f39a22dce12b6a567f70f
帅比Crystal <51915895+CrystalCAI11@users.noreply.github.com>
2023-05-08 16:04:24 +0800

    empty project

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import './screens/products_overview_screen.dart';
-import './screens/product_detail_screen.dart';
-import './providers/products.dart';
-
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => Products(),
-      child: MaterialApp(
-          title: 'MyShop',
-          theme: ThemeData(
-              colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
-                  .copyWith(secondary: Colors.deepOrange),
-              fontFamily: 'Lato'),
-          home: const ProductsOverviewScreen(),
-          routes: {
-            ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
-          }),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('First App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                'Hello World!',
+              ),
+              Text(
+                'It\'s time to learn Flutter!',
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
-
-// class MyHomePage extends StatelessWidget {
-//   const MyHomePage({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('MyShop'),
-//       ),
-//       body: const Center(
-//         child: Text('Let\'s build a shop!'),
-//       ),
-//     );
-//   }
-// }
